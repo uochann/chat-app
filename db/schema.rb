145e25c8cc884ef518fb6c329b676ff25551edb3
uochann <ma602sa@gmail.com>
2020-09-22 12:22:12 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_021040) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
